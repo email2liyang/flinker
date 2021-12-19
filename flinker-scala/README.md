7717beb108c36a07f5187182d7eb20bc2940a1a7
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-19 08:39:16 +0800

    Delete flinker-scala directory

--- flinker-scala/README.md
+++
@@ -1,3 +0,0 @@
-# flinker-scala
-
-* [Beam and Flink Functions](https://www.vipmind.me/data/flink/beam-and-flink-functions.html)
--- flinker-scala/src/main/scala/me/vipmind/flinker/transforms/Models.scala
+++
@@ -1,5 +0,0 @@
-package me.vipmind.flinker.transforms
-
-case class Student(name: String, year: Int, score: Int)
-
-case class Teacher(name: String, year: Int, course: String)
